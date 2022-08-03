9e9820c3c53ba722c0b90f6306649750dbfb841d
DOBEST\tangpeng <2595921323@qq.com>
2022-08-03 18:28:36 +0800

    仿照使用闭包实现观察者模式

--- Assets/Scripts/model/AvatarModel.lua
+++ Assets/Scripts/model/AvatarModel.lua
@@ -40,40 +40,103 @@ function  AvatarModel.initCharacter(dataPart,dataNum)
 		table.insert(AvatarModel.num,v)
 	end
 	for i = 1, #AvatarModel.part do
-		--print(AvatarModel.part[i])
 		AvatarModel:UpdatePart(AvatarModel.part[i],AvatarModel.num[i])
 	end
-	--print(#AvatarModel.part)
+	
 end
+
+
 
 --config Observer
-function AvatarModel:AddObserver(observer)
-	if self.observer==nil then
-		self.observers={}
-	end
-	table.insert(self.observers,observer)
-end
 
-function AvatarModel:RemoveObserver(observer)
-	for k, v in pairs(self.observers) do
-		if observer==v then
-			table.remove(self.observers,k)
-			break
+function creat()
+	local signalToMethod={}
+	
+	local weak={_mode="null"}
+	
+	--add observer
+	local register=function(signal,observer,method)
+		local t=signalToMethod[signal] or {}
+		local ob=observer or weak
+		local meth={method,ob}
+		setmetatable(meth,weak)
+		table.insert(t,meth)
+		signalToMethod[signal]=t
+	end
+	
+	--removeObserver
+	local cancel=function(signal,observer,method)
+		local t=signalToMethod[signal]
+		if not t then
+			return
+		end
+		
+		if not method and not observer then
+			signalToMethod[signal]=nil
+			return
+		end
+		local i,v
+		i=#t
+		while i>0 do
+			v=t[i] or {}
+			if ~method or v[1]==method then
+				if not observer or v[2]==observer then
+					table.remove(t,i)
+				end
+			end
+			i=i-1
 		end
 	end
+	
+	--notify
+	local notify=function(signal,...)
+		local t=signalToMethod[signal]
+		if not t then
+			return
+		end
+		for k, v in pairs(t) do
+			if v[2]==weak then
+				v[1](...)
+			else
+				
+				v[1](...)
+			end
+		end
+	end
+	
+	return register,cancel,notify
 end
 
+function signal(notify,name)
+	return function (...)
+		return notify(name,...)
+	end
+end
+
+
+AvatarModel.reg,AvatarModel.cancel,AvatarModel.notify=creat()
+function AvatarModel:AddObserver(signal,observer,method)
+	if method~=nil then
+		AvatarModel.reg(signal,observer,method)
+	end
+	
+	
+end
+
+function AvatarModel:RemoveObserver(signal,observer,method)
+	if method~=nil then
+		AvatarModel.cancel(signal,observer,method)
+	end
+	
+end
 
 --notify observer
 function AvatarModel:AddNewPart(part)
-	for k, v in pairs(self.observers) do
-		v.AddNewPart(part)
-	end
+	AvatarModel.notify("addNewPart",part)
+
 end
 function AvatarModel:UpdatePart(part,num)
-	for k, v in pairs(self.observers) do
-		v.onUpdatePart(self.state,part,num)
-	end
+	local partNum=part.."-"..num
+	AvatarModel.notify("updatePart",partNum)
 end
-
 return  AvatarModel
